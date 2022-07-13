105bc32f91f4db8069fd65e3b636cf7a8785b949
Maksimchik36 <Peltek1985@gmail.com>
2022-07-13 15:28:51 +0300

    add 100vh

--- src/components/App/App.styled.jsx
+++ src/components/App/App.styled.jsx
@@ -1,14 +1,14 @@
 import styled from "@emotion/styled";
 
 export const Container = styled.div`
-        height: '100vh';
+        height: 100vh;
         margin:0;
         padding: 20px;
-        // display: 'flex';
-        // justify-content: 'center';
-        // align-items: 'center';
-        // font-size: 40;
-        // color: '#010101';
+        display: 'flex';
+        justify-content: 'center';
+        align-items: 'center';
+        font-size: 40;
+        color: '#010101';
         background-image: linear-gradient(to right, #eea2a2 0%, #bbc1bf 19%, #57c6e1 42%, #b49fda 79%, #7ac5d8 100%);
 `;
 
