c31392867e372ec1b9bb871aa7c6ccf9a4a126d6
Maksimchik36 <Peltek1985@gmail.com>
2022-07-29 16:33:32 +0300

    code refactored from class to hooks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,95 +1,76 @@
-import React, {Component} from "react";
+import React, { useState, useEffect } from "react";
 import ContactForm from "../ContactForm";
 import ContactList from "../ContactList";
 import FilterByName from "../FilterByName";
 import {Container, TitleMain, TitleSecondary} from './App.styled';
 
-class App extends Component  {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+const App = () => {
+  
+  const [contacts, setContacts] = useState(JSON.parse(window.localStorage.getItem('contacts')) ?? []);
+  const [filtered, setFiltered] = useState('');
     
-  // получает из localStorage данные о содержимом массива и записывает их в state.contacts
-  componentDidMount() {
-    const data = localStorage.getItem('contacts')
-    const parsedData = JSON.parse(data);
-    // обязательная проверка - существуют ли данные (не равно ли null)
-    if (parsedData) {
-          this.setState({contacts: parsedData});
-        }
-
     
-  }
+    // если contacts изменяется, то при каждом рендере перезаписывает их в localStorage
+  useEffect(() => {
+      window.localStorage.setItem('contacts', JSON.stringify(contacts))    
+  }, [contacts])
 
-  // если данные обновляются, то при каждом рендере перезаписывает их в state.contacts
-  componentDidUpdate(prevProps, prevState) {
-    const { contacts } = this.state;
-    // проверка -  есть ли изменения, и только при положительном ответе производятся какие-либо действия
-    if (prevState.contacts.length !== !contacts.length) {
-      localStorage.setItem('contacts', JSON.stringify(contacts))
-    }
-  }
 
-  // записывает вводимые данные в state.filter
-  handleFilter = (event) => {
+  // записывает вводимые данные в filtered
+  const handleFilter = (event) => {
     const { value } = event.currentTarget;
-    this.setState({ filter: value });
+    setFiltered(value);
   };
   
-  // получает данные из формы и обновляет state.contacts, если такого контакта ещё нет в списке
-  getDataFromContactForm = (data) =>{
+  // получает данные из формы и обновляет contacts, если такого контакта ещё нет в списке
+  const getDataFromContactForm = (data) => {
     // проверяет есть ли добавляемый контакт в списке
-    const contactIsInList = this.state.contacts.some(contact =>contact.name === data.name);
+    const contactIsInList = contacts.some(contact =>contact.name === data.name);
     // вносит новые данные в state
-    const changeState = () => this.setState((prevState)=>{return {contacts:[...prevState.contacts, data]}});
     const errorAdd  = () => alert(`${data.name} is already in contacts list.`);
-    contactIsInList !== true ? changeState() : errorAdd();
+    contactIsInList !== true ? setContacts(prev=>[...prev, data]) : errorAdd();
   }
   
   // получает отфильтрованные контакты
-  getVisibleContacts = ()=>{
-    const {filter, contacts} = this.state;
+  const getVisibleContacts = ()=>{
     // приводит значение filter к нижнему регистру
-    const normalizedFilter = filter.toLowerCase();
+    const normalizedFilter = filtered.toLowerCase();
     
     // фильтрует контакты в зависимости от введённых данных в форме поиска
     return contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter));
   }
 
-  // удаляет контакт из списка, сравнивая id контакта и id
-  deleteContactFromList = (event) =>{
-    const {contacts} = this.state;
-    this.setState({contacts: contacts.filter(contact => contact.id !== event.target.id)})
+  // удаляет контакт из списка, сравнивая id контакта и id нажатой кнопки
+  const deleteContactFromList = (event) =>{
+    const undeletedContacts = contacts.filter(contact => contact.id !== event.target.id);
+    setContacts(undeletedContacts);
   }
 
-
-  render() {
-    const visibleContacts = this.getVisibleContacts();
+  // результат вызова функции записывается в переменную (для облегчения последующего использования в коде)
+  const visibleContacts = getVisibleContacts();
 
   return (
     <Container>
        <TitleMain>Phonebook</TitleMain>
       
       <ContactForm
-        submitForm = {this.getDataFromContactForm}>
+        submitForm = {getDataFromContactForm}>
       </ContactForm>
       
       <TitleSecondary>Contacts</TitleSecondary>
 
       <FilterByName
-        value={this.state.filter}
-        onChange={this.handleFilter}>
+        value={filtered}
+        onChange={handleFilter}>
       </FilterByName>
 
       <ContactList
         array={visibleContacts}
-        onClick={this.deleteContactFromList}
+        onClick={deleteContactFromList}
         >       
       </ContactList>
     </Container>
   )
-  }
 };
 
 export default App;
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,45 +1,55 @@
-import React, {Component} from "react";
+import React, { useState } from "react";
 import { nanoid } from 'nanoid';
 import { Form, Label, Input, Button} from './ContactForm.styled'
 
-class ContactForm extends Component {
-    state = {
-        name: '',
-        number: ''
-      };
+const ContactForm = ({ submitForm }) => {
+   
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-    // Для всех инпутов создаем один обработчик
-  // Различать инпуты будем по атрибуту name
-  handleChange = (event) => {
-    const { name, value } = event.target;
-    this.setState({ [name]: value });
+
+  // изменяет значение name на введённое в инпуте
+  const handleChangeName = (event) => {
+    const { value } = event.target;
+    setName(value);
   };
 
-  handleSubmit = event => {
+
+  // изменяет значение number на введённое в инпуте
+  const handleChangeNumber = (event) => {
+    const { value } = event.target;
+    setNumber(value);
+  };
+
+
+  // отправляет введённые данные в App
+  const handleSubmit = event => {
     event.preventDefault();
     const {name, number} = event.currentTarget;
     const id = nanoid();
-    const newContact = {id:id, name: name.value, number: number.value};
-    this.props.submitForm(newContact);
-    this.reset();
+    const newContact = { id: id, name: name.value, number: number.value };
+    submitForm(newContact);
+    reset();
   }
   
+  
   // очищение инпутов формы
-  reset = () => {
-    this.setState({ name: '', number: ''})
+  const reset = () => {
+    setName('');
+    setNumber('');
   };
-
-    render(){
+  
+  
         return(
-          <Form onSubmit={this.handleSubmit}>
+          <Form onSubmit={handleSubmit}>
             <Label>Name
               <Input 
                 type="text"
                 name="name"
                 pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                 title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                onChange={this.handleChange}
-                value={this.state.name}
+                onChange={handleChangeName}
+                value={name}
                 required/>
             </Label>
             <Label>Number
@@ -48,15 +58,15 @@ class ContactForm extends Component {
                 name="number"
                 pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
                 title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                onChange={this.handleChange}
-                value={this.state.number}
+                onChange={handleChangeNumber}
+                value={number}
                 required
               />
             </Label>
             <Button type="submit">Add contact</Button>
       </Form>
     )
-    }
+    
 }
 
 export default ContactForm;
